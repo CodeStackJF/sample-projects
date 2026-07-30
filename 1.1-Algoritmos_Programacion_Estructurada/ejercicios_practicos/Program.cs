d9598b76a88ec5053c17dcb44e924fddeda23680
AZDEVOPS\jfuentes <AZDEVOPS\jfuentes>
2026-07-30 13:35:44 -0600

    update

--- 1.1-Algoritmos_Programacion_Estructurada/ejercicios_practicos/Program.cs
+++ 1.1-Algoritmos_Programacion_Estructurada/ejercicios_practicos/Program.cs
@@ -10,40 +10,49 @@ class Program
     public static void Ejercicio1()
     {
         string nombre = string.Empty;
+        //Ejecutamos un ciclo infinito hasta que el usuario ingrese un nombre
         while(nombre == string.Empty)
         {
             Console.WriteLine("Escriba su nombre:");
             nombre = Console.ReadLine()!;
         }
 
+        //Declaramos la variable para almacenar las 3 notas
         decimal[] notas = new decimal[3];
         for(int i = 0; i <= 2; i++)
         {
             Console.WriteLine($"Ingrese la nota #{i+1}");
             string sNota = Console.ReadLine()!;
+            //esta variable almacenará el valor ingresado por el usuario solamente si es un numero, caso contrario se quedará como seri
             decimal nota = 0;
+            //el TryParse permite realizar un intento de conversión, si la cadena no es un decimal entonces devuelve false y no almacena ningun valor en nota
             decimal.TryParse(sNota, out nota);
+            //guardamos la nota en la primera posición del arreglo
             notas[i] = nota;
         }
 
         decimal suma_nota = 0;
+        //sumamos las notas
         foreach(decimal nota in notas)
         {
             suma_nota += nota;
         }
+        //obtenemos la cantidad de notas
         decimal promedio = suma_nota / notas.Length;
+        //mostramos el resultado
         Console.WriteLine($"El estudiante ha {(promedio >= 6? "aprobado":"reprobado")} con una nota de {Math.Round(promedio, 2)}");
     }
 
     public static void Ejercicio2()
     {
         int numero = 0;
-        
+        //ejecutamos mientras el valor ingresado no sea un numero entero
         bool isInteger = false;
         while(!isInteger)
         {
             Console.WriteLine("Ingrese un número entero:");
             string sNumero = Console.ReadLine()!;
+            //TryParse intenta convertir la cadena en un numero, si no es compatible devuelve false
             isInteger = int.TryParse(sNumero, out numero);
         }
         Console.WriteLine($"El numero {numero} es {(numero < 0 ? "negativo":"positivo")}");
@@ -53,13 +62,17 @@ class Program
     {
         int numero = 0;
         
+
         bool isInteger = false;
+        //ejecutamos mientras el valor ingresado no sea un numero entero
         while(!isInteger)
         {
             Console.WriteLine("Ingrese un número entero:");
             string sNumero = Console.ReadLine()!;
             isInteger = int.TryParse(sNumero, out numero);
         }
+
+        //mostramos la tabla de multiplicar
         for(int i = 1; i <= 10; i++)
         {
             Console.WriteLine($"{i} x {numero} = {i*numero}");
@@ -71,6 +84,7 @@ class Program
         int edad = 0;
         
         bool isInteger = false;
+        //ejecutamos mientras el valor ingresado no sea un numero entero
         while(!isInteger)
         {
             Console.WriteLine("Ingrese su edad:");
@@ -78,20 +92,25 @@ class Program
             isInteger = int.TryParse(sNumero, out edad);
         }
 
+        //Determinamos si posee licencia o no por medio de una lista de opciones
         string opcion_licencia = string.Empty;
+        //definimos las opciones validas
         string[] opciones = ["S", "N"];
+        //ejecutamos mientras sea un valor vacio o un opción no válida
         while(opcion_licencia == string.Empty)
         {
             Console.WriteLine("¿Posee licencia?:");
             Console.WriteLine("S = Si");
             Console.WriteLine("N = No");
             opcion_licencia = Console.ReadLine()!;
+            //Si el valor ingresado no corresponde a los de la lista de opciones vaciamos la variable para que siga ejecutandose el while
             if(!opciones.Contains(opcion_licencia))
             {
                 opcion_licencia = string.Empty;
             }
         }
 
+        //ejecutamos las condiciones
         if(edad >= 18)
         {
             if(opcion_licencia == "S")
@@ -115,8 +134,9 @@ class Program
 
     public static void Ejercicio5()
     {
-        string[] estudiantes = new string[]
-        {
+        //almacenamos los estudiantes, los indice servirán como llaves para identificarlos
+        string[] estudiantes =
+        [
             "Juan",
             "María",
             "Roxana",
@@ -127,10 +147,12 @@ class Program
             "Pedro",
             "Sofía",
             "Roxana"
-        };
+        ];
 
+        //declaramos una variable bidimensional para guardar las 3 notas de cada estudiante
         double[,] notas = new double[10, 3];
         Random rand = new Random();
+        //generamos valores aleatorios para las notas entre 0 a 10
         for(int i = 0; i <= notas.GetLength(0) - 1; i++)
         {
             notas[i, 0] = rand.NextDouble() * 10;
@@ -138,13 +160,16 @@ class Program
             notas[i, 2] = rand.NextDouble() * 10;
         }
 
+        //obtenemos el promedio de cada estudiante
         for(int i = 0; i <= notas.GetLength(0) - 1; i++)
         {
             double promedio = (notas[i, 0] + notas[i, 1] + notas[i, 2]) / 3;
             Console.WriteLine($"La nota promedio de {estudiantes[i]} es {promedio}");
         }
 
+        //variable temporal para almacenar el estudiante con la nota mayor en cada iteración
         int idEstudiantePromedioMayor = 0;
+        //variable temporal para almacenar la mayor nota encontrada en cada iteración
         double notaMayor = 0;
         for(int i = 0; i <= notas.GetLength(0) - 1; i++)
         {
